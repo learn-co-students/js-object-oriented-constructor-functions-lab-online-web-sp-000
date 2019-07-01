d0cba15c05d51f8952fbf557052632bad212bad4
Daviicode <d.ydzdavid@yahoo.com>
2019-07-01 15:29:16 -0400

    Done.

---
+++ index.js
@@ -0,0 +1,17 @@
+class Scooter(year, color, model) {
+    this.year =  year
+    this.color = color
+    this.model = model
+
+}
+
+class Driver(name, age, experience) {
+    this.name = name
+    this.age = age
+    this.experience = experience
+}
+
+class PickupLocation(address, city) {
+    this.address = address
+    ths.city = city
+}
